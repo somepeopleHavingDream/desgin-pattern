3e810998aa33736ef4eae87dc73ae6b7e1405d49
yangxin <1393003255@qq.com>
2022-07-30 17:12:10 +0800

    style: 更改了一些代码

--- src/main/java/org/yangxin/desginpattern/pattern/structural/decorator/v2/AbstractBatterCake.java
+++ src/main/java/org/yangxin/desginpattern/pattern/structural/decorator/v2/AbstractBatterCake.java
@@ -4,6 +4,7 @@ package org.yangxin.desginpattern.pattern.structural.decorator.v2;
  * @author yangxin
  * 2020/03/09 20:57
  */
+@SuppressWarnings("AlibabaAbstractMethodOrInterfaceMethodMustUseJavadoc")
 public abstract class AbstractBatterCake {
 
     protected abstract String getDescription();
